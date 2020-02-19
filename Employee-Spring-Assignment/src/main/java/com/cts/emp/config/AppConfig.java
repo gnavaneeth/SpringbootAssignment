90327ec7d9ba8e27cc1ff9466f92960603176ac4
gnavaneeth <61177994+gnavaneeth@users.noreply.github.com>
2020-02-19 10:00:25 +0530

    Config changes

--- Employee-Spring-Assignment/src/main/java/com/cts/emp/config/AppConfig.java
+++ Employee-Spring-Assignment/src/main/java/com/cts/emp/config/AppConfig.java
@@ -7,7 +7,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScans(value={@ComponentScan("com.cts.emp.service"),@ComponentScan("com.cts.emp.Dao")})
 public class AppConfig {
-	
-	
 
 }
